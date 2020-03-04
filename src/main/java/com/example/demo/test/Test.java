b70a51b538a937c59a3d4e68b2805967fa90aa6b
wzh <vae226146@163.com>
2020-03-04 16:31:12 +0800

    Test类里写了一个遍历Map的输出信息

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -12,6 +12,13 @@ import java.util.*;
  */
 public class Test {
     public static void main(String[] args) {
-
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "张三");
+        map.put("2", "李四");
+        map.put("3", "王五");
+        for (String m:map.keySet()
+             ) {
+            System.out.println("编号:"+m+"\t"+"姓名:"+map.get(m));
+        }
     }
 }
